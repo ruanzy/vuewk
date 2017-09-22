cee8f747db8af329bc352bad7cbe6f595e2e826b
ruanzy <ruanzhiyong6496@163.com>
2017-09-22 17:49:17 +0800

    动态图表

--- WebRoot/src/page/dash.vue
+++ WebRoot/src/page/dash.vue
@@ -4,54 +4,105 @@
           <li><router-link :to="{path: '/home'}">首页</router-link></li>
           <li>Haddop监控</li>
         </ul>
-        <p>服务器JVM总内存: {{memery.total}}</p>
-        <p>服务器已使用内存: {{memery.total - memery.free}}</p>
-        <div class="p20" style="height: 400px; width: 500px;">
-          <IEcharts :option="bar" :loading="loading"></IEcharts>
+        <div class="p30">
+          <div class="p20" style="height: 350px; width: 600px;border: 1px dashed #ccc;">
+            <IEcharts :option="line" @ready="onReady"></IEcharts>
+          </div>
+          <p>服务器JVM总内存: 11111111</p>
+          <p>服务器已使用内存: 11111111</p>
         </div>
   </div>
 </template>
 <script>
-export default {
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App',
-      memery: {
-        total: 0,
-        free: 0
+  export default {
+    data () {
+      const that = this
+      return {
+        xdata: [],
+        data: [],
+        timer: null,
+        now: +new Date(1997, 9, 3),
+        oneDay: 24 * 3600 * 1000,
+        line: {
+          title: {
+            text: '服务器内存'
+          },
+          tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+              animation: false
+            }
+          },
+          xAxis: {
+            type: 'time',
+            splitLine: {
+              show: false
+            }
+          },
+          yAxis: {
+            type: 'value',
+            boundaryGap: [0, '100%'],
+            splitLine: {
+              show: false
+            }
+          },
+          series: [{
+            name: '模拟数据',
+            type: 'line',
+            showSymbol: false,
+            hoverAnimation: false,
+            data: that.data
+          }]
+        }
+      }
+    },
+    methods: {
+      onReady (instance) {
+        const that = this
+        that.chart = instance
       },
-      bar: {
-        title: {
-          text: 'ECharts Hello World'
-        },
-        tooltip: {},
-        xAxis: {
-          data: ['Shirt', 'Sweater', 'Chiffon Shirt', 'Pants', 'High Heels', 'Socks']
-        },
-        yAxis: {},
-        series: [{
-          name: 'Sales',
-          type: 'bar',
-          data: [5, 20, 36, 10, 10, 20]
-        }]
+      randomData () {
+        const that = this
+        that.now = new Date(+that.now + that.oneDay)
+        return {
+          name: that.now.toString(),
+          value: [
+            [that.now.getFullYear(), that.now.getMonth() + 1, that.now.getDate()].join('/'),
+            Math.round(Math.random()*(100 - 20 + 1) + 20)
+          ]
+        }
+      }
+    },
+    beforeMount () {
+      const that = this
+      for (var i = 0; i < 100; i++) {
+        that.data.push(that.randomData())
+      }
+    },
+    mounted () {
+      const that = this
+      if (that.timer) {
+        clearInterval(that.timer)
+        that.timer = null
+      }
+      that.timer = setInterval(function () {
+        that.data.shift()
+        that.data.push(that.randomData())
+        that.chart && that.chart.setOption({
+          series: [{
+            data: that.data
+          }]
+        })
+      }, 1000)
+    },
+    beforeDestroy () {
+      const that = this
+      if (that.timer) {
+        clearInterval(that.timer)
+        that.timer = null
       }
     }
-  },
-  created () {
-    var params = {page: 1, pagesize: 10};
-    var url = "http://localhost:8089/monitor/memery";
-    this.$http.get(url, {params: params}).then(response => {
-      var data = response.body
-      this.memery.total = data.total
-      this.memery.free = data.free
-    }, response => {
-      // error callback 
-    })
-  },
-  methods: {
-    a: function () {}
   }
-}
 </script>
 <style>
 </style>
