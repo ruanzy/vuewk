200f9784e313b1c6127256ed810b2bacb30e41f8
ruanzy <ruanzhiyong6496@163.com>
2017-09-26 09:41:39 +0800

    优化

--- WebRoot/src/page/dash.vue
+++ WebRoot/src/page/dash.vue
@@ -5,12 +5,20 @@
           <li>Haddop监控</li>
         </ul>
         <div class="p30">
-          <div style="width: 600px;border: 1px dashed #ccc;">
+          <table style="width: 100%;border: 1px dashed #ccc;"><tr>
+          <td style="width: 50%;">
             <center><table width="75%" align="center"><tr><td>最大: {{hmumax}}</td><td>大小: {{hmucommitted}}</td><td>已使用: {{hmuused}}</td></tr></table></center>
             <div style="height: 350px; width: 600px;">
               <IEcharts :option="line" @ready="onReady" theme="macarons"></IEcharts>
             </div>
-          </div>
+          </td>
+          <td>
+            <center><table width="75%" align="center"><tr><td>已启动的总数: {{thread_started}}</td><td>活动: {{thread_active}}</td><td>守护进程: {{thread_daemon}}</td></tr></table></center>
+            <div style="height: 350px; width: 600px;">
+              <IEcharts :option="line2" @ready="onReady2" theme="macarons"></IEcharts>
+            </div>
+          </td>
+          </tr></table>
         </div>
   </div>
 </template>
@@ -24,6 +32,9 @@ import {dateFormat} from '../components/date.js';
         hmucommitted: null,
         hmuused: null,
         hmumax: null,
+        thread_started: null,
+        thread_daemon: null,
+        thread_active: null,
         line: {
             color: ['#6495ed','#2ec7c9','#6495ed',
                       '#ff69b4','#ba55d3','#cd5c5c','#ffa500','#40e0d0',
@@ -64,7 +75,7 @@ import {dateFormat} from '../components/date.js';
               axisLabel : {
                 formatter: function(value, index)
                 {
-                  return 5 * index + "M";
+                  return parseInt(value/1024/1000) + "M";
                 }
               },
               splitArea : {show : true},
@@ -88,6 +99,64 @@ import {dateFormat} from '../components/date.js';
                   data: []
               }
             ]
+		      },
+        line2: {
+            color: ['#6495ed','#2ec7c9','#6495ed',
+                      '#ff69b4','#ba55d3','#cd5c5c','#ffa500','#40e0d0',
+                      '#1e90ff','#ff6347','#7b68ee','#00fa9a','#ffd700',
+                      '#6699FF','#ff6666','#3cb371','#b8860b','#30e0e0'],
+            legend: {
+              y: 'bottom',
+              data:['活动进程', '守护进程']
+            },
+            tooltip: {
+              trigger: 'axis',
+              axisPointer: {
+                animation: false
+              }
+            },
+            calculable : true,
+            xAxis: {
+              type: 'category',
+              data: [],
+              boundaryGap: false,
+              axisLine: {
+                lineStyle: {
+                  color: '#777'
+                }
+              },
+              splitLine: {
+                show: true
+              }
+            },
+            yAxis: {
+              type: 'value',
+              boundaryGap: [0, '100%'],
+              axisLine: {
+                lineStyle: {
+                  color: '#777' 
+                }
+              },
+              splitArea : {show : true},
+              splitLine: {
+                show: true
+              }
+            },
+            series: [{
+                name: '活动进程',
+                type: 'line',
+                showSymbol: false,
+                hoverAnimation: false,
+                data: []
+              },
+              {
+                  name: '守护进程',
+                  type: 'line',
+                  showSymbol: false,
+                  hoverAnimation: false,
+                  data: []
+              }
+            ]
 		      }
         }
     },
@@ -95,6 +164,10 @@ import {dateFormat} from '../components/date.js';
       onReady (instance) {
         const that = this
         that.chart = instance
+      },
+      onReady2 (instance) {
+        const that = this
+        that.chart2 = instance
       },
       update () {
         const that = this;
@@ -120,7 +193,32 @@ import {dateFormat} from '../components/date.js';
         }).catch(function(response) {
           console.log(response)
         });
-      }
+    },
+    update2 () {
+        const that = this;
+        var url = "http://localhost:8089/monitor/thread";
+        var d = new Date();
+        this.$http.post(url, {point: 1}).then(function(response){
+            var data = response.data;
+            var thread_daemon = data.thread_daemon[0];
+            var thread_started = data.thread_started[0];
+            var thread_active = data.thread_active[0];
+            var opt = that.chart2.getOption();
+            var str = dateFormat(d, 'HH:mm:ss');
+            that.thread_daemon = thread_daemon;
+            that.thread_started = thread_started;
+            that.thread_active = thread_active;
+            opt.xAxis[0].data.shift();
+            opt.series[0].data.shift();
+            opt.series[1].data.shift();
+            opt.xAxis[0].data.push(str);
+            opt.series[0].data.push(thread_active);
+            opt.series[1].data.push(thread_daemon);
+            that.chart2.setOption(opt);
+        }).catch(function(response) {
+          console.log(response)
+        });
+    },
     },
     beforeMount () {
         const that = this;
@@ -164,12 +262,54 @@ import {dateFormat} from '../components/date.js';
         }).catch(function(response) {
           console.log(response)
         });
+
+        var url2 = "http://localhost:8089/monitor/thread";
+        this.$http.post(url2, {point: 200}).then(function(response){
+            var data = response.data;
+            var thread_daemon = data.thread_daemon;
+            var thread_started = data.thread_started;
+            var thread_active = data.thread_active;
+            var timestamps = [];
+            var values1 = [];
+            var values2 = [];
+            var size = thread_daemon.length;
+            var num = (size - 1) * 5 * 1000;
+            var t = new Date().getTime() - num;
+            for (var i = 0; i < size; i++) {
+              var d = new Date(t);
+              var str = dateFormat(d, 'HH:mm:ss');
+              timestamps.push(str);
+              values1.push(thread_active[i]);
+              values2.push(thread_daemon[i]);
+              t += 5 * 1000;
+            }
+            that.thread_started = thread_started[size - 1];
+            that.thread_daemon = thread_daemon[size - 1];
+            that.thread_active = thread_active[size - 1];
+            that.chart2.setOption({
+              xAxis: {
+                data: timestamps
+              },
+              series: [{
+                data: values1
+              },{
+                data: values2
+              }]
+            });
+            that.timer2 = setInterval(that.update2, 5000);
+        }).catch(function(response) {
+          console.log(response)
+        });
     },
     beforeDestroy () {
       const that = this
-      if (that.timer) {
-        clearInterval(that.timer)
-        that.timer = null
+      if (that.timer1) {
+        clearInterval(that.timer1)
+        that.timer1 = null
+      }
+      if (that.timer2) {
+        clearInterval(that.timer2)
+        that.timer2 = null
       }
     }
   }
